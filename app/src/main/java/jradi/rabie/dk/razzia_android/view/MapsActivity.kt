fa6e41729e22a313f0d8d738c485c77b98a2b9d7
Rabie Jradi <jradi88@gmail.com>
2018-12-01 23:00:00 +0100

    map activity that launches map page provider

--- app/src/main/java/jradi/rabie/dk/razzia_android/view/MapsActivity.kt
+++ app/src/main/java/jradi/rabie/dk/razzia_android/view/MapsActivity.kt
@@ -1,43 +1,113 @@
-package jradi.rabie.dk.razzia_android
+package jradi.rabie.dk.razzia_android.view
 
+import android.app.IntentService
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
+import android.widget.Toast
+import com.google.android.gms.location.ActivityTransition
+import com.google.android.gms.location.ActivityTransitionResult
+import com.google.android.gms.location.DetectedActivity
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import jradi.rabie.dk.razzia_android.R
+import jradi.rabie.dk.razzia_android.databinding.ActivityMapsBinding
+import jradi.rabie.dk.razzia_android.model.*
+import jradi.rabie.dk.razzia_android.viewmodel.GoogleMapsProvider
+import jradi.rabie.dk.razzia_android.viewmodel.MapViewModel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+//TODO consider making clean interfaces with code that is platform specific (for Android) so that we can reuse logic in a Kotlin Native project so we can support iOS devices
+
+//TODO implement a broadcast receiver that is started on each phone restart in order to subscripe to google activity recognition framework
+
+//FIXME calls to location and recognition services can throw an execption even if we check for permission. we need to wrap them in try catch
+
+
+interface PermissionProviderInterface {
+    suspend fun getLocationPermission(): Boolean
+}
+
+//TODO implement this class so we can handle permission requests
+abstract class PermissionRequestActivity : PermissionProviderInterface, ScopedAppActivity() {
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override suspend fun getLocationPermission(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class MapsActivity : PermissionRequestActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        val binding = DataBindingUtil.setContentView<ActivityMapsBinding>(this, R.layout.activity_maps)
+        val viewModel = MapViewModel()
+        binding.viewModel = viewModel
+
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
+        launch {
+            try {
+                viewModel.init(googleMapsProvider = GoogleMapsProvider(mapFragment), bikeActivityRecognitionClientProviderCreator = BikeActivityRecognitionClientProviderCreator())
+            } catch (e: CancellationException) {
+                showToast(e.message ?: "Something went wrong")
+            }
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+}
+
+class BikeActivityRecognitionService : IntentService("BikeActivityRecognitionService") {
+    private val collisionDetectorProvider = ConfigurationProvider.config.getCollisionDetectorProvider(context = this)
+
+    private fun startCollisionDetectorService() = startService(Intent(this,CollisionDetectorService::class.java))
+
+    override fun onHandleIntent(incomingIntent: Intent?) {
+        val intent = incomingIntent ?: return
+        val result = ActivityTransitionResult.extractResult(intent) ?: return
+        val events = result.transitionEvents ?: return
+
+        //reverse the events order so first index contains latest event
+        val bikeEvents = events.reversed().filter { it.activityType == DetectedActivity.ON_BICYCLE }
+
+        val bikeExitEvents = bikeEvents.filter { it.transitionType == ActivityTransition.ACTIVITY_TRANSITION_EXIT }.mapIndexedNotNull { index, event -> index }
+        val bikeEnterEvents = bikeEvents.filter { it.transitionType == ActivityTransition.ACTIVITY_TRANSITION_ENTER }.mapIndexedNotNull { index, event -> index }
+
+        if (bikeExitEvents.isEmpty() && bikeEnterEvents.isEmpty()) {
+            //Why did we get an intent to handle then? Halt execution.
+            return
+        }
+
+        if (bikeExitEvents.isEmpty() && bikeEnterEvents.isNotEmpty()) {
+            //User started biking
+            startCollisionDetectorService()
+            return
+        }
+        if (bikeExitEvents.isNotEmpty() && bikeEnterEvents.isEmpty()) {
+            collisionDetectorProvider.abort()
+            return
+        }
+        // We have to look at which of the two events occurred the earliest in order to know the user's current activity
+        if (bikeExitEvents.sorted()[0] < bikeEnterEvents.sorted()[0]) {
+            //Stop watching as user have stopped biking
+            collisionDetectorProvider.abort()
+        } else {
+            //Start watching, user has started biking
+            startCollisionDetectorService()
+        }
+        return
+
     }
 }
+
