43509160df0aff0e86e0ac8e0c47c35204b6a19a
Rabie Jradi <jradi88@gmail.com>
2018-12-13 22:40:07 +0100

    implemented coroutine scope for the app's activities

---
+++ app/src/main/java/jradi/rabie/dk/razzia_android/view/ScropedAppActivity.kt
@@ -0,0 +1,29 @@
+package jradi.rabie.dk.razzia_android.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * @author rabie
+ *
+ *
+ */
+abstract class ScopedAppActivity : AppCompatActivity(), CoroutineScope {
+    protected lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Default
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
