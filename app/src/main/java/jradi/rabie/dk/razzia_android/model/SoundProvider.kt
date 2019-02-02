27e4ae586feb33a509f9f063923b35860f435c47
Rabie Jradi <jradi88@gmail.com>
2019-02-02 15:54:41 +0100

    implemented simple notification class

--- app/src/main/java/jradi/rabie/dk/razzia_android/model/SoundProvider.kt
+++ app/src/main/java/jradi/rabie/dk/razzia_android/model/SoundProvider.kt
@@ -1,8 +1,17 @@
 package jradi.rabie.dk.razzia_android.model
 
-import android.util.Log
+import android.app.NotificationManager
+import android.content.Context.NOTIFICATION_SERVICE
+import android.media.RingtoneManager
+import android.net.Uri
+import android.support.v4.app.NotificationCompat
+import jradi.rabie.dk.razzia_android.R
+import jradi.rabie.dk.razzia_android.view.App
+import jradi.rabie.dk.razzia_android.view.logPrint
+import jradi.rabie.dk.razzia_android.view.stringResource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+
 
 /**
  * @author rabie
@@ -10,27 +19,60 @@ import kotlinx.coroutines.withContext
  *
  */
 
-interface SoundProviderInterface {
-    suspend fun makeSound()
+interface AlertProviderInterface {
+    suspend fun alertUser()
 }
 
 /**
  * Class responsible for interacting with Android API so user can get a warning audio sample played
  */
-class AndroidSoundProvider : SoundProviderInterface {
-    suspend override fun makeSound() {
-        withContext(Dispatchers.Main){
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+class AndroidAlertProvider : AlertProviderInterface {
+    suspend override fun alertUser() {
+
     }
 
+}
+
+class NotificationPresenter{
+
+    private val notificationManager = App.appContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+    suspend fun showCollisionServiceIsActive(){
+        showNotification(title = "Collision Service Active", body = "Collision Service Active")
+    }
+
+    suspend fun showNotification(){
+       showNotification(title = stringResource(R.string.app_name), body = stringResource(R.string.app_name))
+    }
+
+    suspend private fun showNotification(title : String, body: String){
+        withContext(Dispatchers.Main) {
+            var soundUri: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+
+            if (soundUri == null) {
+                soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                if (soundUri == null) {
+                    soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+                }
+            }
+
+            val notificationBuilder = NotificationCompat.Builder(App.appContext)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSound(soundUri)
+
+            //TODO this is just test code
+            notificationManager.notify(1,notificationBuilder.build())
+        }
+    }
 }
 
 /**
  * Class used for testing
  */
-class MockedSoundProvider : SoundProviderInterface {
-    suspend override fun makeSound() {
-        Log.d(javaClass.simpleName, "Oh no! The smell of cheese is closing in!")
+class MockedAlertProvider : AlertProviderInterface {
+    suspend override fun alertUser() {
+        logPrint("Oh no! The smell of cheese is closing in!")
     }
 }
