7ebc45a2e55216a9fa092039b8e6d312f988e534
Rabie Jradi <jradi88@gmail.com>
2019-02-02 15:43:37 +0100

    renamed test class

--- app/src/androidTest/java/jradi/rabie/dk/razzia_android/AlertFilterProviderTest.kt
+++ app/src/androidTest/java/jradi/rabie/dk/razzia_android/AlertFilterProviderTest.kt
@@ -1,16 +1,10 @@
 package jradi.rabie.dk.razzia_android
 
-import android.location.Location
 import android.support.test.runner.AndroidJUnit4
-import jradi.rabie.dk.razzia_android.model.AlertProvider
-import jradi.rabie.dk.razzia_android.model.MockedSoundProvider
-import jradi.rabie.dk.razzia_android.model.NaiveFenceCollisionDetector
-import jradi.rabie.dk.razzia_android.model.entities.CircularFence
-import jradi.rabie.dk.razzia_android.model.entities.GPSLocation
-import jradi.rabie.dk.razzia_android.model.entities.Id
+import jradi.rabie.dk.razzia_android.model.AlertFilterProvider
+import jradi.rabie.dk.razzia_android.model.MockedAlertProvider
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -22,37 +16,37 @@ import org.junit.runner.RunWith
  *
  */
 @RunWith(AndroidJUnit4::class)
-class AlertProviderTest {
+class AlertFilterProviderTest {
 
-    private lateinit var alertProvider: AlertProvider
+    private lateinit var alertFilterProvider: AlertFilterProvider
     private val alertThresholdInSeconds = 1
     @Before
     fun setup() {
-        alertProvider = AlertProvider(MockedSoundProvider(), alertThresholdInSeconds)
+        alertFilterProvider = AlertFilterProvider(MockedAlertProvider(), alertThresholdInSeconds)
     }
 
     @Test
     fun oneCallWithAlert() =  runBlocking{
-        val shouldAlertUser = alertProvider.alert()
+        val shouldAlertUser = alertFilterProvider.isAlertAllowed()
         assertTrue(shouldAlertUser)
     }
 
     @Test
     fun twoQuickCallsWithNoSecondAlert() =  runBlocking{
-        var shouldAlertUser = alertProvider.alert()
+        var shouldAlertUser = alertFilterProvider.isAlertAllowed()
         assertTrue(shouldAlertUser)
-        shouldAlertUser = alertProvider.alert()
+        shouldAlertUser = alertFilterProvider.isAlertAllowed()
         //Not enough time has passed
         assertTrue(!shouldAlertUser)
     }
 
     @Test
     fun fiveQuickCallsWithNoSecondAlerts() =  runBlocking{
-        var shouldAlertUser = alertProvider.alert()
+        var shouldAlertUser = alertFilterProvider.isAlertAllowed()
         assertTrue(shouldAlertUser)
 
         repeat(5) {
-            shouldAlertUser = alertProvider.alert()
+            shouldAlertUser = alertFilterProvider.isAlertAllowed()
             //Not enough time has passed
             assertTrue(!shouldAlertUser)
         }
@@ -60,22 +54,22 @@ class AlertProviderTest {
 
     @Test
     fun oneCallThenDelayAbitAndThenCallAgain() = runBlocking {
-        var shouldAlertUser = alertProvider.alert()
+        var shouldAlertUser = alertFilterProvider.isAlertAllowed()
         assertTrue(shouldAlertUser)
 
         delay(alertThresholdInSeconds * 500L)
-        shouldAlertUser = alertProvider.alert()
+        shouldAlertUser = alertFilterProvider.isAlertAllowed()
         //Not enough time has passed
         assertTrue(!shouldAlertUser)
     }
 
     @Test
     fun oneCallThenDelayUntilThresholdAndThenCallAgain() = runBlocking {
-        var shouldAlertUser = alertProvider.alert()
+        var shouldAlertUser = alertFilterProvider.isAlertAllowed()
         assertTrue(shouldAlertUser)
 
         delay(alertThresholdInSeconds * 1000L)
-        shouldAlertUser = alertProvider.alert()
+        shouldAlertUser = alertFilterProvider.isAlertAllowed()
         //enough time has passed
         assertTrue(shouldAlertUser)
     }
