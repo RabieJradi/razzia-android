84554a863c0f07f3257e5b1061d7462b80956ce9
Rabie Jradi <jradi88@gmail.com>
2019-02-02 15:56:57 +0100

    implemented collision service to monitor for potential events

--- app/src/main/java/jradi/rabie/dk/razzia_android/model/services/CollisionDetectorService.kt
+++ app/src/main/java/jradi/rabie/dk/razzia_android/model/services/CollisionDetectorService.kt
@@ -1,2 +1,26 @@
-package jradi.rabie.dk.razzia_android.model.services 
+package jradi.rabie.dk.razzia_android.model.services
 
+import android.app.IntentService
+import android.content.Intent
+import jradi.rabie.dk.razzia_android.model.ConfigurationProvider
+import jradi.rabie.dk.razzia_android.model.NotificationPresenter
+import jradi.rabie.dk.razzia_android.view.logPrint
+import kotlinx.coroutines.runBlocking
+
+/**
+ * When this service is started, it will launch a blocking coroutine which will not release the service Thread unless the coroutine job is cancelled from outside, so the service isn't killed and the coroutine GC'ed.
+ */
+class CollisionDetectorService : IntentService("CollisionDetectorService") {
+
+    private val collisionDetectorProvider = ConfigurationProvider.config.getCollisionDetectorProvider(context = this)
+
+    override fun onHandleIntent(intent: Intent?) = runBlocking<Unit> {
+        //TODO push persistent notification. tell the user that the app is checking for collisions
+        NotificationPresenter().showCollisionServiceIsActive()
+        logPrint("[CollisionDetectorService] onHandleIntent called")
+        collisionDetectorProvider.watch()
+        logPrint("[CollisionDetectorService] Stopped observing for collisions")
+        stopSelf() //Kill this service as we the watching have been cancelled anyway.
+        //TODO stop persistent notification
+    }
+}
