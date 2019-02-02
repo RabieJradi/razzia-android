9c87603f36d75b38457763fd303c8977a4c2c859
Rabie Jradi <jradi88@gmail.com>
2019-02-02 15:50:03 +0100

    renamed class

--- app/src/main/java/jradi/rabie/dk/razzia_android/model/AlertFilterProvider.kt
+++ app/src/main/java/jradi/rabie/dk/razzia_android/model/AlertFilterProvider.kt
@@ -1,18 +1,18 @@
 package jradi.rabie.dk.razzia_android.model
 
 
-interface AlertProviderInterface {
-    suspend fun alert(): Boolean
+interface AlertFilterProviderInterface {
+    suspend fun isAlertAllowed(): Boolean
 }
 
 //TODO consider adding a push notification provider, so the user has a fast way to enter the app
 /**
- * Responsible for suppressing a request to alert the user about a collision event if user already have had an alert not too long ago.
+ * Responsible for suppressing a request to isAlertAllowed the user about a collision event if user already have had an isAlertAllowed not too long ago.
  */
-class AlertProvider(private val alertThresholdInSeconds: Int = 30) : AlertProviderInterface {
+class AlertFilterProvider(private val alertThresholdInSeconds: Int = 30) : AlertFilterProviderInterface {
     private var systemTimeSinceLastAlert : Long = 0
 
-    override suspend fun alert(): Boolean {
+    override suspend fun isAlertAllowed(): Boolean {
         System.currentTimeMillis()
         val secondsAgoSinceLastAlert = (System.currentTimeMillis() - systemTimeSinceLastAlert).div(1000)
         if (secondsAgoSinceLastAlert >= alertThresholdInSeconds) {
